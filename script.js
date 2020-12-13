f8f546422242d2e8d65d8a92aa699c5aa93a4e5e
chad <canddx3@gmail.com>
2020-12-12 21:51:21 -0600

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,79 @@
 // Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
+fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+   response.json().then(function (json) {
+      const missionTarget = document.getElementById("missionTarget")
+      missionTarget.innerHTML = 
+      `<div>
+      <h2>Mission Destination</h2>
+      <ol>
+         <li>Name: ${json[4].name}</li>
+         <li>Diameter: ${json[4].diameter}</li>
+         <li>Star: ${json[4].star}</li>
+         <li>Distance from Earth: ${json[4].distance}</li>
+         <li>Number of Moons: ${json[4].moons}</li>
+      </ol>
+      <img src="${json[4].image}">
+      </div>`
+   });
+});
+
+let pilotName = document.querySelector("input[name=pilotName]");
+let copilotName = document.querySelector("input[name=copilotName]");
+let fuelLevel = document.querySelector("input[name=fuelLevel]");
+let cargoMass = document.querySelector("input[name=cargoMass]");
+let launchStatus = document.getElementById("launchStatus");
+let faultyItems = document.getElementById("faultyItems");
+let pilotStatus = document.getElementById("pilotStatus");
+let copilotStatus = document.getElementById("copilotStatus");
+let fuelStatus = document.getElementById("fuelStatus");
+let cargoStatus = document.getElementById("cargoStatus");
+
+let form = document.getElementById("launchForm");
+form.addEventListener("submit", (event) => {
+
+  const letters = /^[a-zA-Z]+$/;
+  const numbers = /^[0-9]+$/;
+   if (pilotName.value === '' || !letters.test(pilotName.value)) {
+      alert("please enter a Pilots name");
+   }
+
+   if (copilotName.value === '' || !letters.test(copilotName.value)) {
+      alert("please enter a Copilots name");
+   } 
+   
+   if (!numbers.test(fuelLevel.value)) {
+      alert("please enter a valid fuel level number")
+   } 
+   if (!numbers.test(cargoMass.value)) {
+      alert("please enter a valid cargo mass number")
+   }
+   event.preventDefault();
+
+   if (fuelLevel.value <=10000) {
+      faultyItems.style.visibility = "visible";
+      launchStatus.innerText = 'Shuttle not ready for launch';
+      launchStatus.style.color = 'red';
+      pilotStatus.innerText = `${pilotName.value} Ready`;
+      copilotStatus.innerText = `${copilotName.value} Ready`;
+      fuelStatus.innerText = 'Not enough fuel';
+   } else if
+      (cargoMass.value > 10000) {
+      faultyItems.style.visibility = "visible";
+      launchStatus.innerText = 'Shuttle not ready for launch';
+      launchStatus.style.color = 'red';
+      pilotStatus.innerText = `${pilotName.value} Ready`;
+      copilotStatus.innerText = `${copilotName.value} Ready`;
+      cargoStatus.innerText = 'To much weight';
+   } else {
+      faultyItems.style.visibility = "visible";
+      launchStatus.innerText = 'Shuttle is ready to launch';
+      launchStatus.style.color = 'green';
+      pilotStatus.innerText = `${pilotName.value} Ready`;
+      copilotStatus.innerText = `${copilotName.value} Ready`;
+      console.log(pilotName.value)
+
+   }
+});
+   
